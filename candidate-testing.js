cec3ac7ee119d8f7a812a60f373f8bb17db984b6
Rania Abdalla <>
2021-06-03 19:24:37 +0000

    finish part1

--- candidate-testing.js
+++ candidate-testing.js
@@ -5,8 +5,8 @@ const input = require('readline-sync');
 // TODO 1.1a: Define candidateName // 
 let candidateName;
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
+let question = "Who was the first American woman in space? ";
+let correctAnswer = "Sally Ride";
 let candidateAnswer;
 let questions;
 let correctAnswers;
@@ -15,13 +15,12 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-
+ candidateName = input.question("Please enter your name: ");
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
-
+candidateAnswer = input.question(question);
 }
 
 function gradeQuiz(candidateAnswers) {
@@ -30,7 +29,11 @@ function gradeQuiz(candidateAnswers) {
 
 
   let grade;
-  
+  if (candidateAnswer === correctAnswer){
+    console.log("correct");
+  }else {
+    console.log("wrong answer");
+  }
 
   return grade;
 }
@@ -38,7 +41,7 @@ function gradeQuiz(candidateAnswers) {
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
-  
+  console.log("Hi,",candidateName );
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
