9bf76193284f01810c753b6828beaa283e9a54c5
Rania Abdalla <52862597+raniabdalla@users.noreply.github.com>
2021-06-10 03:08:14 +0000

    finished part2

--- candidate-testing.js
+++ candidate-testing.js
@@ -8,9 +8,9 @@ let candidateName;
 let question = "Who was the first American woman in Space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer;
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions =  ['Who was the first American woman in space?', 'True or false: 5 kilometer == 5000 meters?', '(5 + 3)/2 * 10 = ?', 'Given the array [8, \'Orbit\', \'Trajectory\', 45], what entry is at index 2?', 'What is the minimum crew size for the ISS?'];
+let correctAnswers = ['Sally Ride','true', '40', 'Trajectory', '3' ];
+let candidateAnswers= [];
 
 
 function askForName() {
@@ -20,7 +20,10 @@ function askForName() {
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-candidateAnswer = input.question(question);
+for(let i=0; i<questions.length; i++){
+    candidateAnswers[i] = input.question(questions[i]);
+  }
+  //candidateAnswer = input.question(question);
 }
 
 function gradeQuiz(candidateAnswers) {
@@ -29,11 +32,9 @@ function gradeQuiz(candidateAnswers) {
 
 
   let grade;
-  if (candidateAnswer === correctAnswer){
-    console.log("correct");
-  }else {
-    console.log("wrong answer");
-  }
+    for(let i=0; i<questions.length; i++){
+      console.log(`your answer: ${candidateAnswers[i]}\ncorrect answer: ${correctAnswers[i]}\n`)
+    }
 
   return grade;
 }
